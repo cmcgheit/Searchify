63a22216b2d1eea9a730e0978feb66335d98f6ca
YOUR NAME <YOUR EMAIL ADDRESS>
2017-05-03 17:33:16 -0500

    initial commit

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+target 'Searchify' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Searchify
+  pod 'Alamofire', '~> 4.4'
+  pod 'ChameleonFramework/Swift', :git => 'https://github.com/ViccAlexander/Chameleon.git'
+
+  target 'SearchifyTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ Searchify/AudioVC.swift
@@ -0,0 +1,78 @@
+//
+//  AudioVC.swift
+//  Searchify
+//
+//  Created by C McGhee on 5/3/17.
+//  Copyright © 2017 Fam Design. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+
+class AudioVC : UIViewController {
+    
+    var image = UIImage()
+    var mainSongTitle = String()
+    var mainPreviewURL = String()
+    
+    @IBOutlet var background: UIImageView!
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var songTitle: UILabel!
+    @IBOutlet weak var playpausebtn: UIButton!
+    
+    override func viewDidLoad() {
+        self.navigationItem.title = "Searchify - Song Preview"
+        self.navigationController?.hidesNavigationBarHairline = true
+        
+        songTitle.text = mainSongTitle
+        background.image = image
+        mainImageView.image = image
+        
+        downloadFileFromURL(url: URL(string: mainPreviewURL)!)
+        
+        playpausebtn.setTitle("Pause Preview", for: .normal)
+        
+    }
+    
+    func downloadFileFromURL(url: URL) {
+        var downloadTask = URLSessionDownloadTask()
+        downloadTask = URLSession.shared.downloadTask(with: url, completionHandler: {
+            customURL, response, error in
+            
+            self.play(url: customURL!)
+            
+        })
+        
+        downloadTask.resume()
+        
+    }
+    
+    func play(url: URL) {
+        
+        do {
+        
+        player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+            player.play()
+        }
+        catch{
+            print(error)
+    }
+  }
+    
+
+    @IBAction func pauseplay(_ sender: Any) {
+        if player.isPlaying {
+            player.pause()
+            playpausebtn.setTitle("Crank It", for: .normal)
+            
+        }
+        else{
+            player.play()
+            playpausebtn.setTitle("Shut It Off", for: .normal)
+        }
+    }
+    
+}
