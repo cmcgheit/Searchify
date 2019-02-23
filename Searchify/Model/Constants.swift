b435b0f861e99ec46be9c98cc616ea0ed7208d8e
cmcgheit <cmcgheit@cs-imac.local>
2019-02-22 20:32:36 -0600

    updated readme // fldr struct

---
+++ Searchify/Model/Constants.swift
@@ -0,0 +1,27 @@
+//
+//  Constants.swift
+//  Searchify
+//
+//  Created by C McGhee on 8/31/17.
+//  Copyright © 2017 Fam Design. All rights reserved.
+//
+
+import Foundation
+
+let kCloudRailAPIKey = "590b5b837f28d7017656f8b0"
+
+let VIMEO_TOKEN_URL = "https://api.vimeo.com/oauth/access_token"
+
+let VIMEO_AUTH_URL = "https://api.vimeo.com/oauth/authorize"
+
+let vimeoClientID = "f31ab4ff2d686d8236342505b4d539da3c1105e7"
+
+let vimeoClientSecret = "0DhpuPLZEyBKKqnTN03Yi4YKh0G1e/2x3AsQ3HERAe9tg0TbR9bo9g3E6lTgiC72ZXcLr/ZMXSbHqVQLClrmHUWvQrnQkTsC8dMeVA6LL9eev+DSR2hR6aQpVw3J2YEQ"
+
+let youTubeClientID = ""
+
+let youTubeRedirectURI = "com.cloudrail.example:/auth"
+
+
+
+
